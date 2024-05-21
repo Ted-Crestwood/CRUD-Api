e396c8b9e782c0cf6b6b74b48bde27e84c315e3a
Ted@Crestwood <ted@crestwood.co.ke>
2024-05-21 15:40:30 +0300

    Update application.controller.js

--- controllers/application.controller.js
+++ controllers/application.controller.js
@@ -1,5 +1,6 @@
 const Application = require("../models/application.model");
 
+
 const getApplications = async (req, res) => {
     try {
         const application = await Application.find({})
@@ -8,18 +9,15 @@ const getApplications = async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 }
+
+
 const createApplication = async (req, res) => {
     try {
-        const existingEmail = await Application.findOne({ email: req.body.email });
-        if (existingEmail) {
-            return res.status(400).json({ message: "Email already exists" });
-        }
         const application = await Application.create(req.body);
+        console.log("application:",application)
         res.status(200).json(application);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
-
-
 module.exports = { createApplication, getApplications };
